f121f0c3ef09a180ac18f4d326a0109351fea9f0
nobo <505931977@qq.com>
2018-07-26 16:47:19 +0800

    Added      .gitignore Added      client.js Added      package-lock.json Added      package.json Added      server.js Added      xuser.thrift

---
+++ client.js
@@ -0,0 +1,42 @@
+var thrift = require('thrift');
+
+var UserService = require('./gen-nodejs/UserService.js');
+var ttypes = require('./gen-nodejs/xuser_types');
+
+
+
+var connection = thrift.createConnection('127.0.0.1', 3000);
+var client = thrift.createClient(UserService, connection);
+
+connection.on("error", function (e) {
+  console.log(e);
+});
+
+
+var x = new ttypes.User({
+  uid: '112',
+  uname: 'aab',
+  usex: 0,
+  uage: '181'
+});
+
+
+client.add(x, function (err, res) {
+  console.log("ADD OK1");
+  client.get('112', function (err, res) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("Res:", res);
+      connection.end();
+    }
+  });
+  client.adduname('112', function (err, res) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("Res:", res);
+      connection.end();
+    }
+  });
+});
